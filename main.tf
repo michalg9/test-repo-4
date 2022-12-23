e46cbea85f31931e78acace3ea5e003b4518e5d6
Michał Goliński <3693729+michalg9@users.noreply.github.com>
2022-12-23 11:32:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "random_password" "password" {
 }
 
 resource "random_password" "password2" {
-  length           = 24
+  length           = 25
   special          = true
   override_special = "_%@"
 }
